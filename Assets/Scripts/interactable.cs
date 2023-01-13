8a9a1ce3a0a9ddd89c856656a0a01b5c97c1dbb3
uhveahn <thearia27@gmail.com>
2023-01-13 18:15:26 +0800

    figured out press E (kinda)
    if u press E near the door console will contain text

--- Assets/Scripts/interactable.cs
+++ Assets/Scripts/interactable.cs
@@ -2,25 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class door : MonoBehaviour
+public class interactable : MonoBehaviour
 {
-
-    [SerializeField] BoxCollider2D _collider;
-
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    public void OnTriggerEnter2D()
-    {
-
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            print("You hab oepend the duur");
+        }
     }
 }
